375ed380ecf19eeb0921078d8a7c84c4f375e965
Caleb Smith <smithcaleb042@gmail.com>
2021-03-08 16:10:19 -0700

    added validation

--- todos.js
+++ todos.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const { body, validationResult } = require("express-validator");
+const TodoList = require("./lib/todolist");
 
 const app = express();
 const host = "localhost";
@@ -12,6 +16,20 @@ app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false })); //tells express abou the format used by the form data. it is URL-encoded for this app
+app.use(session({
+  name: "launch-school-todos-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+}));
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 const sortTodoLists = lists => {
   const compareByTitle = (todoListA, todoListB) => {
@@ -35,8 +53,48 @@ const sortTodoLists = lists => {
 };
 
 app.get("/", (req, res) => {
-  res.render("lists", { todoLists: sortTodoLists(todoLists) });
+  res.redirect("lists");
 });
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+app.get("/lists", (req, res) => {
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  })
+});
+
+app.post("/lists",
+  [
+    body("todoListTitle")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("The list title is required.")
+      .isLength({ max: 100 })
+      .withMessage("List title must be between 1 and 100 characters.")
+      .custom(title => {
+        let duplicate = todoLists.find(list => list.title === title);
+        return duplicate === undefined;
+      })
+      .withMessage("List title must be unique."),
+  ], 
+  (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      errors.array().forEach(message => req.flash("error", message.msg));
+      res.render("new-list", {
+        flash: req.flash(), 
+        todoListTitle: req.body.todoListTitle,
+      });
+    } else {
+      todoLists.push(new TodoList(req.body.todoListTitle));
+      req.flash("success", "The todo list has been created");
+      res.redirect("/lists");
+    }
+  }
+);
 
 app.listen(port, host, () => {
   console.log(`Todos is listening on port ${port} of ${host}!`);
