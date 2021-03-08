d6242fbf0cc4c1e7380919486eccff4f08b5ae40
Caleb Smith <smithcaleb042@gmail.com>
2021-03-08 13:16:03 -0700

    added sorting to the todolists

--- todos.js
+++ todos.js
@@ -5,13 +5,37 @@ const app = express();
 const host = "localhost";
 const port = 3000;
 
+let todoLists = require("./lib/seed-data");
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public"));
+
+const sortTodoLists = lists => {
+  const compareByTitle = (todoListA, todoListB) => {
+    let titleA = todoListA.title.toLowerCase();
+    let titleB = todoListB.title.toLowerCase();
+
+    if (titleA < titleB) {
+      return -1;
+    } else if (titleA > titleB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  let undone = lists.filter(todoList => !todoList.isDone());
+  let done   = lists.filter(todoList => todoList.isDone());
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+  return [].concat(undone, done);
+};
 
 app.get("/", (req, res) => {
-  res.render("lists");
+  res.render("lists", { todoLists: sortTodoLists(todoLists) });
 });
 
 app.listen(port, host, () => {
